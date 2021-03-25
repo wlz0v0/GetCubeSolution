1b4cbb2f2315206562ca4ba1e0ea7dd0a4b4fe2f
wlz0v0 <75922528+wlz0v0@users.noreply.github.com>
2021-03-25 09:25:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import translate
 import recognition
 
 
@@ -18,4 +19,38 @@ string = recognition.get_solution()
 cubeStr: str = string
 req: Dict = collection.find_one({"cube_str": cubeStr})
 solution: List = req["pass"]
-print(solution)
+
+commandList: List = []
+for commandChar in solution:
+    if commandChar == 'u':
+        translate.cube_twist_clockwise(commandList)
+
+    elif commandChar == 'cu':
+        translate.cube_twist_anti_clockwise(commandList)
+
+    elif commandChar == 'r':
+        translate.cube_spin_right(commandList)
+        translate.cube_up_turn_forward(commandList)
+        translate.cube_twist_anti_clockwise(commandList)
+        translate.cube_down_turn_forward(commandList)
+        translate.cube_spin_left(commandList)
+
+    elif commandChar == 'cr':
+        translate.cube_spin_right(commandList)
+        translate.cube_up_turn_forward(commandList)
+        translate.cube_twist_clockwise(commandList)
+        translate.cube_down_turn_forward(commandList)
+        translate.cube_spin_left(commandList)
+
+    elif commandChar == 'b':
+        translate.cube_up_turn_forward(commandList)
+        translate.cube_twist_anti_clockwise(commandList)
+        translate.cube_down_turn_forward(commandList)
+
+    elif commandChar == 'cb':
+        translate.cube_up_turn_forward(commandList)
+        translate.cube_twist_clockwise(commandList)
+        translate.cube_down_turn_forward(commandList)
+
+for i in commandList:
+    print(i)
